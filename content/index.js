c01ad16b3574f4f2b60c9ba8ee02286cdc9dc776
Benjamin Young <byoung2@wiley.com>
2018-06-13 09:43:27 -0400

    Get rid of the nitfiness; use ye olde DOM
    Works better for more people, so win.

--- content/index.js
+++ content/index.js
@@ -1,18 +1,21 @@
 // Uses the PonyMail API to gather remote list of recent messages
 const recent_messages_url = 'https://lists.apache.org/api/stats.lua?list=dev&domain=annotator.apache.org';
 
-const Item = ({id, subject}) => `
-<a class="item" rel="external" href="https://lists.apache.org/thread.html/${id}">${subject}</a>
-`;
+let a = document.createElement('a');
+a.className = 'item';
+a.rel = 'external';
 
-const $h = document.getElementById('recent-mailings');
+const $recent = document.getElementById('recent-mailings');
 
 fetch(recent_messages_url)
   .then(r => r.json())
   .then((msgs) => {
     if ('emails' in msgs && msgs.emails.length > 0) {
       for (let i = 0; i < 5; i++) {
-        $h.insertAdjacentHTML('afterend', Item(msgs.emails[i]));
+        let _a = a.cloneNode(a);
+        _a.href = 'https://lists.apache.org/thread.html/' + msgs.emails[i].id;
+        _a.textContent = msgs.emails[i].subject;
+        $recent.append(_a);
       }
     }
   });
