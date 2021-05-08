6c6dcc297b6b4504ad327e753da4b27ebb1d3479
Gerben <gerben@treora.com>
2021-05-08 17:15:13 +0200

    Include TypeDoc-generated API docs
    With our own theme

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,8 @@ module.exports = function(eleventyConfig) {
     // '...relative to this file': '...relative to `dir.output` below'
     // Built demo content gets copied in verbatim
     'annotator/web/dist/': 'demo/',
+    // Built API docs for each module get copied in verbatim
+    'annotator/docs': 'docs/api',
     // also the images, css, and non-11ty js files
     'src/.htaccess': '.htaccess',
     'src/annotator-icon.svg': 'annotator-icon.svg',
